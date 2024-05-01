8f40d0b6360e59aacc23b96bfcd665cd648abf3b
godsakani <nyonggodwill11@gmail.com>
2024-05-01 15:23:59 +0100

    Feat: Added new utils files

---
+++ mobile_frontend/bloomsai/lib/app/common/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static Color primaryColor = const Color(0xff1C7C45);
+  static Color whiteColor = const Color(0xffFFFFFF);
+  static Color black = const Color(0xff242222);
+  static Color black38 = Colors.black38;
+  static Color black87 = Colors.black87;
+  static Color grey = Colors.grey;
+  static Color greyShade3 = Colors.grey.shade300;
+  static Color red = Colors.red;
+}
---
+++ mobile_frontend/bloomsai/lib/app/common/utils/constant.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:bloomsai/app/common/theme/app_colors.dart';
+import 'package:bloomsai/app/common/theme/text_style.dart';
+import 'package:flutter/material.dart';
+
+class Constants {
+ static void snackMessage({required String message, required BuildContext context}) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    dismissDirection: DismissDirection.up,
+    behavior: SnackBarBehavior.floating,
+    content: Text(message,
+        style: AppTextStyles.small12.copyWith(color: AppColors.whiteColor)),
+    backgroundColor: AppColors.primaryColor,
+  ));
+}
+
+static void snackErrorMessage({required String message,required BuildContext context}) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      message,
+      style: AppTextStyles.small12.copyWith(color: AppColors.whiteColor)
+    ),
+    backgroundColor: AppColors.red,
+  ));
+}
+}
