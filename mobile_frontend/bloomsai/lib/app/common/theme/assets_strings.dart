3d58655301b9be3f0e5812ba25db80ec77147215
godsakani <nyonggodwill11@gmail.com>
2024-05-01 15:23:30 +0100

    Feat: Added new assets (fonts and images), added new packages

---
+++ mobile_frontend/bloomsai/lib/app/common/theme/assets_strings.dart
@@ -0,0 +1,9 @@
+String IMAGE_ASSETS = "assets";
+
+class ImageAssets {
+  static String logo = "$IMAGE_ASSETS/images/logo.svg";
+  static String logo_white = "$IMAGE_ASSETS/images/logo_white.svg";
+  static String onboard = "$IMAGE_ASSETS/images/onboard.png";
+  static String google = "$IMAGE_ASSETS/images/google.svg";
+  static String email = "$IMAGE_ASSETS/images/mail.svg";
+}
