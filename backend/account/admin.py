258e67caa1cf8c25e5e3c6ac54733aae82c43dd5
Divina Mbel <divinamanye@gmail.com>
2024-05-04 04:56:50 +0100

    some points

--- backend/account/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import CustomUser, Profile  # Import your models here
-
-admin.site.register(CustomUser)
-admin.site.register(Profile)
